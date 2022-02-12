48a120cdab39a5428fea2f25a737125c7adb2fda
Jonathas Tavares <jonathasl.tavares@gmail.com>
2022-02-11 21:23:59 -0300

    sessões por filme implementadas

--- src/Menus.java
+++ src/Menus.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Menus {
 	Filme filmes[] = new Filme[5];
-	Sessao sessoes[] = new Sessao[5];
+	Sessao sessoes[] = new Sessao[25];
 	Carrinho carrinho = new Carrinho();
 	Filme filme = new Filme();
 	Scanner menu = new Scanner(System.in);
@@ -53,27 +53,27 @@ public class Menus {
 		case 1:
 			carrinho.setTitulo(filmes[0].getTitulo());
 			carrinho.setDuracao(filmes[0].getDuracao());
-			menuSessoes();
+			menuSessoes1();
 			break;
 		case 2:
 			carrinho.setTitulo(filmes[1].getTitulo());
 			carrinho.setDuracao(filmes[1].getDuracao());
-			menuSessoes();
+			menuSessoes2();
 			break;
 		case 3:
 			carrinho.setTitulo(filmes[2].getTitulo());
 			carrinho.setDuracao(filmes[2].getDuracao());
-			menuSessoes();
+			menuSessoes3();
 			break;
 		case 4:
 			carrinho.setTitulo(filmes[3].getTitulo());
 			carrinho.setDuracao(filmes[3].getDuracao());
-			menuSessoes();
+			menuSessoes4();
 			break;
 		case 5:
 			carrinho.setTitulo(filmes[4].getTitulo());
 			carrinho.setDuracao(filmes[4].getDuracao());
-			menuSessoes();
+			menuSessoes5();
 			break;
 		default:
 			System.out.println("Opção inválida.");
@@ -81,11 +81,11 @@ public class Menus {
 		}
 	}
 
-	public void menuSessoes() {
+	public void menuSessoes1() {
 		int i = 1;
-		for(Sessao se:sessoes) {
-   		 System.out.println("- Sessão "+i+" - \nPreço: R$"+se.getPreco());
-   		 System.out.println("Hora: "+se.getHora()+"\nIngressos dispovíveis: "+se.getDisponiveis());
+		for (int j = 0; j < 5; j++) {
+   		 System.out.println("- Sessão "+i+" de "+filmes[0].getTitulo()+" - \nPreço: R$"+sessoes[j].getPreco());
+   		 System.out.println("Hora: "+sessoes[j].getHora()+"\nIngressos dispovíveis: "+sessoes[j].getDisponiveis());
    		 System.out.println("--------------------------------");
    		 i++;
    	 }
@@ -134,6 +134,215 @@ public class Menus {
 		}
 	}
 	
+	public void menuSessoes2() {
+		int i = 1;
+		for (int j = 5; j < 10; j++) {
+			System.out.println("- Sessão "+i+" de "+filmes[1].getTitulo()+" - \nPreço: R$"+sessoes[j].getPreco());
+	   		 System.out.println("Hora: "+sessoes[j].getHora()+"\nIngressos dispovíveis: "+sessoes[j].getDisponiveis());
+	   		 System.out.println("--------------------------------");
+	   		 i++;
+   	 }
+		System.out.println("0 - Voltar \n");
+		System.out.println("Escolha uma opção: \n");
+		opcao = menu.nextInt();
+        
+		switch (opcao) {
+		case 0:
+			menuFilmesEmCartaz();
+			break;
+		case 1:
+			carrinho.setPreco(sessoes[0].getPreco());
+			carrinho.setHora(sessoes[0].getHora());
+			controle = 5;
+			comprarIngresso();
+			break;
+		case 2:
+			carrinho.setPreco(sessoes[1].getPreco());
+			carrinho.setHora(sessoes[1].getHora());
+			controle = 6;
+			comprarIngresso();
+			break;
+			
+		case 3:
+			carrinho.setPreco(sessoes[2].getPreco());
+			carrinho.setHora(sessoes[2].getHora());
+			controle = 7;
+			comprarIngresso();
+			break;
+		case 4:
+			carrinho.setPreco(sessoes[3].getPreco());
+			carrinho.setHora(sessoes[3].getHora());
+			controle = 8;
+			comprarIngresso();
+			break;
+		case 5:
+			carrinho.setPreco(sessoes[4].getPreco());
+			carrinho.setHora(sessoes[4].getHora());
+			controle = 9;
+			comprarIngresso();
+			break;
+		default:
+			System.out.println("Opção inválida.");
+			menuPrincipal();
+		}
+	}
+	public void menuSessoes3() {
+		int i = 1;
+		for (int j = 10; j < 15; j++) {
+			System.out.println("- Sessão "+i+" de "+filmes[2].getTitulo()+" - \nPreço: R$"+sessoes[j].getPreco());
+	   		 System.out.println("Hora: "+sessoes[j].getHora()+"\nIngressos dispovíveis: "+sessoes[j].getDisponiveis());
+	   		 System.out.println("--------------------------------");
+	   		 i++;
+   	 }
+		System.out.println("0 - Voltar \n");
+		System.out.println("Escolha uma opção: \n");
+		opcao = menu.nextInt();
+        
+		switch (opcao) {
+		case 0:
+			menuFilmesEmCartaz();
+			break;
+		case 1:
+			carrinho.setPreco(sessoes[0].getPreco());
+			carrinho.setHora(sessoes[0].getHora());
+			controle = 10;
+			comprarIngresso();
+			break;
+		case 2:
+			carrinho.setPreco(sessoes[1].getPreco());
+			carrinho.setHora(sessoes[1].getHora());
+			controle = 11;
+			comprarIngresso();
+			break;
+			
+		case 3:
+			carrinho.setPreco(sessoes[2].getPreco());
+			carrinho.setHora(sessoes[2].getHora());
+			controle = 12;
+			comprarIngresso();
+			break;
+		case 4:
+			carrinho.setPreco(sessoes[3].getPreco());
+			carrinho.setHora(sessoes[3].getHora());
+			controle = 13;
+			comprarIngresso();
+			break;
+		case 5:
+			carrinho.setPreco(sessoes[4].getPreco());
+			carrinho.setHora(sessoes[4].getHora());
+			controle = 14;
+			comprarIngresso();
+			break;
+		default:
+			System.out.println("Opção inválida.");
+			menuPrincipal();
+		}
+	}
+	
+	public void menuSessoes4() {
+		int i = 1;
+		for (int j = 15; j < 20; j++) {
+			System.out.println("- Sessão "+i+" de "+filmes[3].getTitulo()+" - \nPreço: R$"+sessoes[j].getPreco());
+	   		 System.out.println("Hora: "+sessoes[j].getHora()+"\nIngressos dispovíveis: "+sessoes[j].getDisponiveis());
+	   		 System.out.println("--------------------------------");
+	   		 i++;
+   	 }
+		System.out.println("0 - Voltar \n");
+		System.out.println("Escolha uma opção: \n");
+		opcao = menu.nextInt();
+        
+		switch (opcao) {
+		case 0:
+			menuFilmesEmCartaz();
+			break;
+		case 1:
+			carrinho.setPreco(sessoes[0].getPreco());
+			carrinho.setHora(sessoes[0].getHora());
+			controle = 15;
+			comprarIngresso();
+			break;
+		case 2:
+			carrinho.setPreco(sessoes[1].getPreco());
+			carrinho.setHora(sessoes[1].getHora());
+			controle = 16;
+			comprarIngresso();
+			break;
+			
+		case 3:
+			carrinho.setPreco(sessoes[2].getPreco());
+			carrinho.setHora(sessoes[2].getHora());
+			controle = 17;
+			comprarIngresso();
+			break;
+		case 4:
+			carrinho.setPreco(sessoes[3].getPreco());
+			carrinho.setHora(sessoes[3].getHora());
+			controle = 18;
+			comprarIngresso();
+			break;
+		case 5:
+			carrinho.setPreco(sessoes[4].getPreco());
+			carrinho.setHora(sessoes[4].getHora());
+			controle = 19;
+			comprarIngresso();
+			break;
+		default:
+			System.out.println("Opção inválida.");
+			menuPrincipal();
+		}
+	}
+	public void menuSessoes5() {
+		int i = 1;
+		for (int j = 20; j < 25; j++) {
+			System.out.println("- Sessão "+i+" de "+filmes[4].getTitulo()+" - \nPreço: R$"+sessoes[j].getPreco());
+	   		 System.out.println("Hora: "+sessoes[j].getHora()+"\nIngressos dispovíveis: "+sessoes[j].getDisponiveis());
+	   		 System.out.println("--------------------------------");
+	   		 i++;
+   	 }
+		System.out.println("0 - Voltar \n");
+		System.out.println("Escolha uma opção: \n");
+		opcao = menu.nextInt();
+        
+		switch (opcao) {
+		case 0:
+			menuFilmesEmCartaz();
+			break;
+		case 1:
+			carrinho.setPreco(sessoes[0].getPreco());
+			carrinho.setHora(sessoes[0].getHora());
+			controle = 20;
+			comprarIngresso();
+			break;
+		case 2:
+			carrinho.setPreco(sessoes[1].getPreco());
+			carrinho.setHora(sessoes[1].getHora());
+			controle = 21;
+			comprarIngresso();
+			break;
+			
+		case 3:
+			carrinho.setPreco(sessoes[2].getPreco());
+			carrinho.setHora(sessoes[2].getHora());
+			controle = 22;
+			comprarIngresso();
+			break;
+		case 4:
+			carrinho.setPreco(sessoes[3].getPreco());
+			carrinho.setHora(sessoes[3].getHora());
+			controle = 23;
+			comprarIngresso();
+			break;
+		case 5:
+			carrinho.setPreco(sessoes[4].getPreco());
+			carrinho.setHora(sessoes[4].getHora());
+			controle = 24;
+			comprarIngresso();
+			break;
+		default:
+			System.out.println("Opção inválida.");
+			menuPrincipal();
+		}
+	}
 	public void comprarIngresso() {
 		System.out.println("- CARRINHO DE COMPRAS -");
 		System.out.println("Filme: "+carrinho.getTitulo());
@@ -157,7 +366,7 @@ public class Menus {
 		
 		switch(opcao) {
 			case 0:
-				menuSessoes();
+				menuFilmesEmCartaz();
 				break;
 				
 			default:
@@ -207,7 +416,7 @@ public class Menus {
 		filmes[4].titulo = "Eternos";
 		filmes[4].duracao = "2:37 horas";
 		
-		
+		//sessoes homem aranha
 		sessoes[0] = new Sessao();
 		sessoes[0].hora = "15:30";
 		sessoes[0].preco = 25.0;
@@ -232,6 +441,110 @@ public class Menus {
 		sessoes[4].hora = "20:00";
 		sessoes[4].preco = 29.0;
 		sessoes[4].disponiveis = 49;
+		
+		//sessoes matrix
+		sessoes[5] = new Sessao();
+		sessoes[5].hora = "13:15";
+		sessoes[5].preco = 20.0;
+		sessoes[5].disponiveis = 20;
+		
+		sessoes[6] = new Sessao();
+		sessoes[6].hora = "14:30";
+		sessoes[6].preco = 18.0;
+		sessoes[6].disponiveis = 60;
+		
+		sessoes[7] = new Sessao();
+		sessoes[7].hora = "20:52";
+		sessoes[7].preco = 22.0;
+		sessoes[7].disponiveis = 54;
+		
+		sessoes[8] = new Sessao();
+		sessoes[8].hora = "22:00";
+		sessoes[8].preco = 24.0;
+		sessoes[8].disponiveis = 23;
+		
+		sessoes[9] = new Sessao();
+		sessoes[9].hora = "13:50";
+		sessoes[9].preco = 17.0;
+		sessoes[9].disponiveis = 55;
+		
+		//sessoes turma da monica
+		sessoes[10] = new Sessao();
+		sessoes[10].hora = "12:30";
+		sessoes[10].preco = 15.0;
+		sessoes[10].disponiveis = 23;
+		
+		sessoes[11] = new Sessao();
+		sessoes[11].hora = "15:30";
+		sessoes[11].preco = 18.0;
+		sessoes[11].disponiveis = 32;
+		
+		sessoes[12] = new Sessao();
+		sessoes[12].hora = "16:00";
+		sessoes[12].preco = 19.0;
+		sessoes[12].disponiveis = 45;
+		
+		sessoes[13] = new Sessao();
+		sessoes[13].hora = "17:30";
+		sessoes[13].preco = 20.0;
+		sessoes[13].disponiveis = 32;
+		
+		sessoes[14] = new Sessao();
+		sessoes[14].hora = "16:30";
+		sessoes[14].preco = 25.0;
+		sessoes[14].disponiveis = 54;
+		
+		//sessoes duna
+		sessoes[15] = new Sessao();
+		sessoes[15].hora = "12:35";
+		sessoes[15].preco = 17.0;
+		sessoes[15].disponiveis = 28;
+		
+		sessoes[16] = new Sessao();
+		sessoes[16].hora = "15:50";
+		sessoes[16].preco = 18.0;
+		sessoes[16].disponiveis = 36;
+		
+		sessoes[17] = new Sessao();
+		sessoes[17].hora = "16:30";
+		sessoes[17].preco = 17.0;
+		sessoes[17].disponiveis = 50;
+		
+		sessoes[18] = new Sessao();
+		sessoes[18].hora = "14:30";
+		sessoes[18].preco = 18.0;
+		sessoes[18].disponiveis = 29;
+		
+		sessoes[19] = new Sessao();
+		sessoes[19].hora = "19:30";
+		sessoes[19].preco = 25.0;
+		sessoes[19].disponiveis = 23;
+		
+		//sessoes eternos
+		sessoes[20] = new Sessao();
+		sessoes[20].hora = "13:35";
+		sessoes[20].preco = 15.0;
+		sessoes[20].disponiveis = 24;
+		
+		sessoes[21] = new Sessao();
+		sessoes[21].hora = "14:50";
+		sessoes[21].preco = 19.0;
+		sessoes[21].disponiveis = 57;
+		
+		sessoes[22] = new Sessao();
+		sessoes[22].hora = "16:35";
+		sessoes[22].preco = 22.0;
+		sessoes[22].disponiveis = 54;
+		
+		sessoes[23] = new Sessao();
+		sessoes[23].hora = "14:50";
+		sessoes[23].preco = 18.0;
+		sessoes[23].disponiveis = 43;
+		
+		sessoes[24] = new Sessao();
+		sessoes[24].hora = "19:50";
+		sessoes[24].preco = 23.0;
+		sessoes[24].disponiveis = 23;
 		
 		}
 }
